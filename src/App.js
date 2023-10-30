b690627d68f6039d660581ad9688438441e0fe57
Chidoskii <51214655+Chidoskii@users.noreply.github.com>
2023-10-29 19:46:11 -0700

    initial set up

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+//pages and components
+import Home from './pages/Home';
+import Navi from './components/Navi';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Navi />
+        <div className="pages">
+          <Routes>
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Navi.js
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const Navi = () => {
+  return (
+    <header>
+      <div className="container">
+        <Link to="/">
+          <h1> Runner Health</h1>
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navi;
